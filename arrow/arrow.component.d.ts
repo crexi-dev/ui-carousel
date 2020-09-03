0b41f514e7bf21c7f60810564f3345f1884889a6
Neil Soult <neil@crexi.com>
2020-09-03 12:07:43 -0700

    cleanup fork

--- arrow/arrow.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { EventEmitter } from '@angular/core';
-export declare class ArrowComponent {
-    dir: string;
-    disabled: boolean;
-    click: EventEmitter<any>;
-    constructor();
-    onClick(): void;
-}
